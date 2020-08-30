187267cf1694ae1665a1ec36b64d7b0e3c1c79bf
Ariane Evans <ariane.evans@student.makeschool.com>
2020-08-30 15:34:20 -0700

    [add] base of code

--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,59 @@
 class Blockchain(object):
+
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_blocks(self):
+    def new_block(self, proof, previous_hash=None):
         #This function creates new blocks and then adds to the existing chain
-        pass
+        """This method will contain two parameters proof, previous hash"""
+        
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'proof': proof,
+            previous_hash: previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Set the current transaction list to empty.
+        self.current_transactions = []
+        self.chain.append(block)
+
+        return block        
 
     def new_transaction(self):
         #This function adds a new transaction to already existing transactions
-        pass
+        """This will create a new transaction which will be sent to the next block. It will contain
+        three variables including sender, recipient and amount"""
+
+        self.current_transactions.append(
+            {
+                'sender': sender,
+                'recipient': recipient,
+                'amount': amount,
+            }
+        )
+
+        return self.last_block['index'] + 1
 
     @staticmethod
 
     def hash(block):
         #Used for hashin a block
-        pass
+        """The following code will create a SHA-256 block hash and also ensure that the dictionary is ordered"""
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
 
     def last_block(self):
         # Calls and returns the last block of the chain
-        pass         
+
+        return self.chain[-1]
+
 
 
     
